c0bd4ee8c849784898344e15a761279bd40a1d3e
Ganesh MS <ganeshms@live.com>
2020-11-06 18:03:13 -0500

    added comment

--- src/main/java/com/registrationlogin/RegistrationLoginApplication.java
+++ src/main/java/com/registrationlogin/RegistrationLoginApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegistrationLoginApplication {
-
+	//Entry level code
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationLoginApplication.class, args);
 	}
