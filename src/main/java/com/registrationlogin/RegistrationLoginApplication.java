04abc38d356a3b46508c9d8573cad071b21b0e33
Madhu Jayarama <madhu.gowda.j@gmail.com>
2020-11-06 18:10:37 -0500

    added madhu comment

--- src/main/java/com/registrationlogin/RegistrationLoginApplication.java
+++ src/main/java/com/registrationlogin/RegistrationLoginApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegistrationLoginApplication {
-
+	// Madhu comment
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationLoginApplication.class, args);
 	}
